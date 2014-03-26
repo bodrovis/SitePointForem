2b925472f71bb24f111b9b9daa4c3bd8b0921b3f
bodrovis <twillight_church@bk.ru>
2014-03-26 15:26:55 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SitepointForem::Application.config.secret_token = 'b919c395c930750bc6f5895b566c931f0d3d592a5543ae1ef25e7fbb3a4a7e75f436882bcaa9b9a7cf7e78b86e2956cfa1d8cdfb932799c1946b1363d6bb1ea5'
